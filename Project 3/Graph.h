8277d27fc1ce7898b43c4042532560e7a778048e
estefanaltabeno <62262717+estefanaltabeno@users.noreply.github.com>
2023-08-02 18:10:26 -0400

    Update Graph.h

--- Project 3/Graph.h
+++ Project 3/Graph.h
@@ -16,9 +16,4 @@ class Graph
 };
 
 
-void Graph::Graph()
-{
-	//use this default constructor to make the graph object, taking in the csv files we discussed on discord and reading all of the airports and their IATA codes, sifting through it all.
-	//I'll be working on this more on 8/2/23, not today but just writing in some key details that I don't want to forget, especially concerning this portion of the work. 
-}
 

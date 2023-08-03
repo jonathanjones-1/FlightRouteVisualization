3199c09a57b65aef8de103992f9f0cf19b1e7239
estefanaltabeno <62262717+estefanaltabeno@users.noreply.github.com>
2023-08-02 21:38:40 -0400

    Update Graph.cpp

--- Project 3/Graph.cpp
+++ Project 3/Graph.cpp
@@ -2,31 +2,75 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cctype>
 
-using namespace std;
+using namespace std
 
-
-Graph::Graph()
+Graph::Graph() 
 {
-	ifstream airportsFile; 
-	
-	string IATA;
+    
+    ifstream airportFile("airports.csv");
 
-	airportsFile.open("airports.csv");
-
-	while (!airportsFile.eof())
-	{
-			getline(airportsFile, IATA, ','); 
-			if (IATA.size() == 3)
+    if (airportFile.is_open()) 
 			{
-					Airport (
-			}
-	}
+        string line;
 
-	       
-	//ifstream routes; 
+        //Skip the beginning row
+        getline(airportFile, line);
 
+        while (getline(airportFile, line)) 
+						{
+            istringstream iss(line);
+            string airportName;
+            string latitude;
+            string longitude;
+            string iataCode;
 
+            if (getline(iss, airportName, ',') && getline(iss, latitude, ',') && getline(iss, longitude, ',') && getline(iss, iataCode, ',')) 
+										{
+                
+                if (!iataCode.empty()) 
+													{
+                    Airport airport(iataCode, latitude, longitude);
+                    adjList[airport];
+                }
+            }
+        }
 
+        airportFile.close();
+    }
+
+    
+    ifstream routeFile("routes.csv");
+
+    if (routeFile.is_open()) 
+				{
+        string line
+        getline(routeFile, line);
+
+        while (getline(routeFile, line)) 
+							{
+            istringstream iss(line);
+            string sourceAirportName;
+            string destinationAirportName;
+
+            if (getline(iss, sourceAirportName, ',') && getline(iss, destinationAirportName, ',')) 
+											{
+                
+                // Find the corresponding Airport objects in the adjList and update the adjacency accordingly
+                auto sourceAirportIt = findAirportByName(sourceAirportName);
+                auto destinationAirportIt = findAirportByName(destinationAirportName);
+
+                if (sourceAirportIt != adjList.end() && destinationAirportIt != adjList.end()) {
+                    adjList[sourceAirportIt->first].insert(destinationAirportIt->first);
+                    adjList[destinationAirportIt->first].insert(sourceAirportIt->first);
+                }
+            }
+        }
+
+        routeFile.close();
+    }
 }
+
+
 

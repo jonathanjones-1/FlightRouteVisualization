eab34398ad9bd28801734542f3dd6ca7dd670e30
estefanaltabeno <62262717+estefanaltabeno@users.noreply.github.com>
2023-08-03 11:04:04 -0400

    Update Graph.h

--- Project 3/Graph.h
+++ Project 3/Graph.h
@@ -13,8 +13,10 @@ private:
 	unordered_map<Airport, unordered_set<Airport, Airport::Hash>, Airport::Hash> adjList;
 public:
 	Graph();
-	/*
+	
 	double haversineCalc(double lat1, lat2, lat3, lat4);
+
+        /*
 
 	void dijkstraAlg(); //left it as void as I will figure out what to   	return late
 	*/

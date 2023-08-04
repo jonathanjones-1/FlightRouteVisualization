03933837fb7f26940c593674e24fde2e75476cea
estefanaltabeno <62262717+estefanaltabeno@users.noreply.github.com>
2023-08-03 23:34:20 -0400

    Update main.cpp
    I'll make an outside function in a bit

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -9,8 +9,31 @@ int main()
 {
 
     Graph testGraph;
-    // testGraph.printGraph();
+    testGraph.printGraph();
 
+
+			string test1 = "LAX"; 
+			string test2 = "JFK";
+
+			auto start = high_resolution_clock::now(); 
+
+			int iataCodeCount = testGraph.BFS(test1, test2); 
+		
+			auto stop = high_resolution_clock::now(); 
+
+			// Using chrono to time the algorithms, feel free to change       
+    // milliseconds here to nanoseconds if needed. 
+
+			auto duration = duration_cast <milliseconds>(stop - start); 
+
+    cout << "Breadth-first search executed in " << duration.count() << "milliseconds!" << endl;
+			cout << "Breadth-first search's path included " << iataCodeCount << " pairs!" << endl;  
+
+
+
+			auto start = high_resolution_clock::now();
+			
+			iataCodeCount = testGraph.
 			
 
     return 0;
